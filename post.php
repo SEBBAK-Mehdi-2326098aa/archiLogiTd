68a8f0dde6cf1b69dce0c67d9e48b93b72100b36
Sebbak <mehdi.sebbak@etu.univ-amu.fr>
2025-02-03 21:44:23 +0100

    Avancée du td

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect('localhost', 'root', '', 'blog_db');
+$link = mysqli_connect('mysql-sebbak.alwaysdata.net', 'sebbak_annonces', 'sebbakAnnonces', 'sebbak_annonces_db');
     
     $result = mysqli_query($link, 'SELECT * FROM Post WHERE id='.$_GET['id'] );
     $post = mysqli_fetch_assoc($result);
