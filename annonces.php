835cd8b1e0f921e6d10e33db9a474ab9ed871745
Sebbak <mehdi.sebbak@etu.univ-amu.fr>
2025-02-06 14:11:59 +0100

    adaptation du code pour que cela fonctionne avec le nouveau format de fichier, le controller ne contient plus d'affichage comme souhaité

--- annonces.php
+++ annonces.php
@@ -1,25 +1,32 @@
 <?php
-    $link = mysqli_connect('mysql-sebbak.alwaysdata.net', 'sebbak_annonces', 'sebbakAnnonces', 'sebbak_annonces_db');
+$link = mysqli_connect('mysql-sebbak.alwaysdata.net', 'sebbak_annonces', 'sebbakAnnonces', 'sebbak_annonces_db');
 
-    $query= 'SELECT login FROM Users WHERE login="'.$_POST['login'].'" and password="'.$_POST['password'].'"';
-    $resultlogin = mysqli_query($link, $query );
+$query = 'SELECT login FROM Users WHERE login="' . $_POST['login'] . '" and password="' . $_POST['password'] . '"';
+$resultlogin = mysqli_query($link, $query);
 
-    if( mysqli_num_rows( $resultlogin) ){
-        $login = $_POST['login'];
+if (mysqli_num_rows($resultlogin)) {
+    $login = $_POST['login'];
 
-        mysqli_free_result( $resultlogin );
-        $resultall = mysqli_query($link, 'SELECT id, title FROM Post');
 
-        $annonces = array();
-        while ($row = mysqli_fetch_row($resultall)) {
-            $annonces[] = $row;
-        }
+    mysqli_free_result($resultlogin);
+    $resultall = mysqli_query($link, 'SELECT id, title FROM Post');
 
+    $annonces = array();
+    while ($row = mysqli_fetch_assoc($resultall)) {
+        $annonces[] = $row;
     }
+}
+else {
+    header('refresh:5;url=index.php');
+    echo 'Erreur de login et/ou mot de passe. Vous allez être redirigé vers la page de login.';
+    exit();
+}
+
+
+mysqli_close($link);
+
+// on inclut le code de la vue
+require 'view/annonces.php';
 ?>
 
 
-
-<?php
-    mysqli_close( $link );
-?>
--- view/annonces.php
+++ view/annonces.php
@@ -1,11 +1,3 @@
-<?php
-if (!isset( $login) or $login == '') {
-    header('refresh:5;url=index.php');
-    echo 'Erreur de login et/ou mot de passe (redirection automatique dans 5 sec.';
-    exit;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
